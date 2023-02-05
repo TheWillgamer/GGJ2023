9dd5fe45d5b4ee0a14b841ea2e8ce46e1d167559
William Jeon <iamwilly99@gmail.com>
2023-02-05 12:31:10 -0800

    updated projectile

--- Assets/Scripts/PlayerBullet.cs
+++ Assets/Scripts/PlayerBullet.cs
@@ -5,16 +5,51 @@ using UnityEngine;
 public class PlayerBullet : MonoBehaviour
 {
     public float speed;
+    public float lifeSpan;
+    private float deathTime;
+    private bool active;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        active = true;
+        deathTime = Time.time + lifeSpan;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        if (Time.time > deathTime)
+        {
+            Destroy(gameObject);
+        }
+        if (active)
+            MoveProjectile();
+    }
+
+    void MoveProjectile()
+    {
+        //Determine how far object should travel this frame.
+        float travelDistance = (speed * Time.deltaTime);
+
+        //Explode bullet if it goes through the wall
+        RaycastHit hit;
+        // Does the ray intersect any walls
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, travelDistance))
+        {
+            if (hit.transform.gameObject.tag == "Enemy")
+            {
+                //hit.transform.gameObject.GetComponent<EnemyHitDetection>().TakeDamage(damage);
+                Invoke("DestroyProjectile", 1.5f);
+                active = false;
+            }
+        }
+
+        transform.position += transform.forward * travelDistance;
+    }
+
+    void DestroyProjectile()
+    {
+        Destroy(transform.parent.gameObject);
     }
 }
