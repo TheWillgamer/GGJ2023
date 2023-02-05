ca69f4a75af52820f7d7ab39c8cf090f77a1bc35
William Jeon <iamwilly99@gmail.com>
2023-02-04 17:18:36 -0800

    rhythm working

--- Assets/Scripts/NoteBehavior.cs
+++ Assets/Scripts/NoteBehavior.cs
@@ -1,18 +1,50 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NoteBehavior : MonoBehaviour
 {
     public bool right;
     public float speed;
+    public float timeToDisappear;
+    private bool disappearing;
+    private float timer;
+
+    void Start()
+    {
+        disappearing = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
         if (right)
             transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
         else
             transform.Translate(-Vector3.right * speed * Time.deltaTime, Space.World);
+
+        if (!disappearing && ((right && transform.localPosition.x > 0f) || (!right && transform.localPosition.x < 0f)))
+        {
+            disappearing = true;
+            timer = 0f;
+            StartCoroutine(Fade());
+        }
+
+        if (disappearing && timer > timeToDisappear)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public IEnumerator Fade()
+    {
+        Color c = GetComponent<Image>().color;
+        for (float alpha = 1f; alpha >= 0; alpha -= 0.01f)
+        {
+            c.a = alpha;
+            GetComponent<Image>().color = c;
+            yield return null;
+        }
     }
 }
--- Assets/Scripts/RhythmManager.cs
+++ Assets/Scripts/RhythmManager.cs
@@ -1,34 +1,66 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Security.Cryptography;
-using System.Threading;
 using UnityEngine;
 
 public class RhythmManager : MonoBehaviour
 {
+    // Note Detection
+    public Transform leftNote;
+    public Transform rightNote;
+
+    // Spawning
+    public Transform hitLoc;        // where the notes collide
     public Transform leftSpawner;
     public Transform rightSpawner;
     public Transform note;
     public float bpm;
+    public float spawnDelay;
+    private float spawnRate;
     private float timer;
 
     void Start()
     {
-        timer = 0f;
+        timer = 0f - spawnDelay;
+        spawnRate = 1f / (bpm / 60);
     }
 
     // Update is called once per frame
     void Update()
     {
+        SpawnNotes();
+        UpdateClosestNodes();
+    }
+
+    void SpawnNotes()
+    {
         timer += Time.deltaTime;
 
-        if(timer > bpm / 60)
+        if (timer > spawnRate)
         {
-            timer -= bpm / 60;
+            timer -= spawnRate;
             var lNote = Instantiate(note, leftSpawner.position, Quaternion.identity);
-            lNote.transform.parent = leftSpawner;
             var rNote = Instantiate(note, rightSpawner.position, Quaternion.identity);
-            rNote.transform.parent = leftSpawner;
+            lNote.transform.SetParent(hitLoc);
+            rNote.transform.SetParent(hitLoc);
+            lNote.transform.localScale = new Vector3(1f, 1f, 1f);
+            rNote.transform.localScale = new Vector3(1f, 1f, 1f);
+            lNote.GetComponent<NoteBehavior>().right = true;
         }
+    }
+
+    void UpdateClosestNodes()
+    {
+        for(int i = 0; i < hitLoc.childCount; i++)
+        {
+            Transform hit = hitLoc.GetChild(i);
+
+            // For closest left node
+            if(hit.localPosition.x < 0f && hit.localPosition.x > -100f && (leftNote == null || leftNote.localPosition.x < hit.localPosition.x))
+                leftNote = hit;
+
+            // For closest right node
+            else if (hit.localPosition.x > 0f && hit.localPosition.x < 100f && (rightNote == null || rightNote.localPosition.x > hit.localPosition.x))
+                rightNote = hit;
+        }
+
+        Debug.Log(leftNote);
     }
 }
